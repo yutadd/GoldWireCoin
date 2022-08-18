792c792524a69f303a4a3e8e12076de0badacda0
yutadd <spyk4m49@gmail.com>
2022-08-19 07:35:47 +0900

    Update for work at school

--- kernel.cu
+++ kernel.cu
@@ -1,6 +1,4 @@
-﻿
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+﻿#include "device_launch_parameters.h"
 #include <assert.h>
 #include <stdio.h>
 #include <cuda.h>
@@ -16,14 +14,13 @@
 
 #define ROTLEFT(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
 #define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
-
 #define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
 #define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 #define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
 #define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
 #define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
 #define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
-
+#define BCD(c) 5 * (5 * (5 * (5 * (5 * (5 * (5 * (5*(5*(c&512)+(c&256))+(c&128))+(c&64))+(c&32))+(c&16))+(c&8))+(c&4))+(c&2))+(c&1)
 /**************************** DATA TYPES ****************************/
 typedef unsigned char BYTE; // 8-bit byte
 typedef uint32_t WORD;		// 32-bit word, change to "long" for 16-bit machines
@@ -60,6 +57,53 @@ char* print_sha(BYTE* buff);
 __device__ void sha256_init(SHA256_CTX* ctx);
 __device__ void sha256_update(SHA256_CTX* ctx, const BYTE data[], size_t len);
 __device__ void sha256_final(SHA256_CTX* ctx, BYTE hash[]);
+__device__ int isspace(unsigned char c) {
+	return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
+}
+__device__ char* trim(char* str) {
+	size_t len = 0;
+	char* frontp = str;
+	char* endp = NULL;
+
+	if (str == NULL) { return NULL; }
+	if (str[0] == '\0') { return str; }
+	for (int len = 0; str[len] != '\0'; len++) {
+		if (str[len] != ' ') {
+			endp = str + len;
+			break;
+		}
+	}
+	endp = str + len;
+
+	/* Move the front and back pointers to address the first non-whitespace
+	 * characters from each end.
+	 */
+	while (isspace((unsigned char)*frontp)) { ++frontp; }
+	if (endp != frontp)
+	{
+		while (isspace((unsigned char)*(--endp)) && endp != frontp) {}
+	}
+
+	if (str + len - 1 != endp)
+		*(endp + 1) = '\0';
+	else if (frontp != str && endp == frontp)
+		*str = '\0';
+
+	/* Shift the string so that it starts at str so that if it's dynamically
+	 * allocated, we can still free it on the returned pointer.  Note the reuse
+	 * of endp to mean the front of the string buffer now.
+	 */
+	endp = str;
+	if (frontp != str)
+	{
+		while (*frontp) { *endp++ = *frontp++; }
+		*endp = '\0';
+	}
+
+
+	return str;
+}
+
 
 char* hash_to_string(BYTE* buff)
 {
@@ -76,10 +120,11 @@ char* hash_to_string(BYTE* buff)
 
 __device__ void sha256_transform(SHA256_CTX* ctx, const BYTE data[])
 {
+
 	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 	WORD S[8];
 
-	// mycpy32(S, ctx->state);
+	//mycpy32(S, ctx->state);
 
 #pragma unroll 16
 	for (i = 0, j = 0; i < 16; ++i, j += 4)
@@ -99,8 +144,7 @@ __device__ void sha256_transform(SHA256_CTX* ctx, const BYTE data[])
 	h = ctx->state[7];
 
 #pragma unroll 64
-	for (i = 0; i < 64; ++i)
-	{
+	for (i = 0; i < 64; ++i) {
 		t1 = h + EP1(e) + CH(e, f, g) + dev_k[i] + m[i];
 		t2 = EP0(a) + MAJ(a, b, c);
 		h = g;
@@ -121,6 +165,7 @@ __device__ void sha256_transform(SHA256_CTX* ctx, const BYTE data[])
 	ctx->state[5] += f;
 	ctx->state[6] += g;
 	ctx->state[7] += h;
+
 }
 
 __device__ void sha256_init(SHA256_CTX* ctx)
@@ -140,12 +185,13 @@ __device__ void sha256_init(SHA256_CTX* ctx)
 __device__ void sha256_update(SHA256_CTX* ctx, const BYTE data[], size_t len)
 {
 	WORD i;
-	for (i = 0; i < len; ++i)
-	{
+
+	// for each byte in message
+	for (i = 0; i < len; ++i) {
+		// ctx->data == message 512 bit chunk
 		ctx->data[ctx->datalen] = data[i];
 		ctx->datalen++;
-		if (ctx->datalen == 64)
-		{
+		if (ctx->datalen == 64) {
 			sha256_transform(ctx, ctx->data);
 			ctx->bitlen += 512;
 			ctx->datalen = 0;
@@ -158,20 +204,22 @@ __device__ void sha256_final(SHA256_CTX* ctx, BYTE hash[])
 	WORD i;
 
 	i = ctx->datalen;
-	if (ctx->datalen < 56)
-	{
+
+	// Pad whatever data is left in the buffer.
+	if (ctx->datalen < 56) {
 		ctx->data[i++] = 0x80;
 		while (i < 56)
 			ctx->data[i++] = 0x00;
 	}
-	else
-	{
+	else {
 		ctx->data[i++] = 0x80;
 		while (i < 64)
 			ctx->data[i++] = 0x00;
 		sha256_transform(ctx, ctx->data);
 		memset(ctx->data, 0, 56);
 	}
+
+	// Append to the padding the total message's length in bits and transform.
 	ctx->bitlen += ctx->datalen * 8;
 	ctx->data[63] = ctx->bitlen;
 	ctx->data[62] = ctx->bitlen >> 8;
@@ -183,8 +231,9 @@ __device__ void sha256_final(SHA256_CTX* ctx, BYTE hash[])
 	ctx->data[56] = ctx->bitlen >> 56;
 	sha256_transform(ctx, ctx->data);
 
-	for (i = 0; i < 4; ++i)
-	{
+	// Since this implementation uses little endian byte ordering and SHA uses big endian,
+	// reverse all the bytes when copying the final state to the output hash.
+	for (i = 0; i < 4; ++i) {
 		hash[i] = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 4] = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 8] = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
@@ -194,6 +243,7 @@ __device__ void sha256_final(SHA256_CTX* ctx, BYTE hash[])
 		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
 	}
+
 }
 
 #define checkCudaErrors(x)                                                    \
@@ -204,17 +254,23 @@ __device__ void sha256_final(SHA256_CTX* ctx, BYTE hash[])
 		if (err != cudaSuccess)                                               \
 			printf("GPU: cudaError %d (%s)\n", err, cudaGetErrorString(err)); \
 	}
-__global__ void sha256_cuda(char* data, int len, long long int* result_nans, BYTE* result)
+__global__ void sha256_cuda(BYTE* data, int len, BYTE* result)
 {
+	//source: src_block
+	// 　　　　不定長
+	//result: nans&hash(sha256)
+	//      2進化10進数整数(10byte)　32byte
+	//　　　　↑固定長↑
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	//文字列に乱数を挿入する。
-	char* mae = "";
+	BYTE* mae = (BYTE*)malloc((len + 10) * sizeof(BYTE));
+	mae = (BYTE*)malloc((len + 10) * sizeof(BYTE));
 	int conma = 0;
 	int index_ = 0;
 	//,が２回出現したらその場所をindex_に保存してループを抜ける。
-	for (index_ = 0; index_ < len; index_++)
+	for (index_ = 0; /*index_ < len*/; index_++)
 	{
-		mae[index_] = (char)data[index_];
+		mae[index_] = (BYTE)data[index_];
 		if (data[index_] == ',')
 		{
 			conma += 1;
@@ -226,75 +282,83 @@ __global__ void sha256_cuda(char* data, int len, long long int* result_nans, BYT
 
 	curand_init((unsigned long long)clock64() + i, 0, 0, &s);
 
-	char* str = (char*)malloc(32);
+	BYTE* str = (BYTE*)malloc(32);
 	int rand = curand_uniform(&s) * 1000000000;
-	result_nans[i] = rand;
 	//数字を文字列にしちゃうやつ。
 	int da = 0;
 	for (da = 0; rand != 0; da++)
 	{
 		int rem = rand % 10;
-		str[da] = (char)(rem + '0');
+		str[da] = (BYTE)(rem + '0');
 		rand = rand / 10;
 	}
-	int srclen = 0;
-	for (srclen = 0; str[srclen] != '\0'; srclen++);
+
+	int strlen = 0;
+	for (strlen = 0; str[strlen] != '\0'; strlen++);
 	//乱数の文字数を取得する。
-	for (int a = 0; a < srclen; a++)
-	{ // maeにsrcをくっつける
-		mae[index_ + a] = str[a];
+	for (int a = 0; a < strlen; a++)
+	{ // maeにstrをくっつける
+		mae[index_ + a + 1] = str[a];
 	}
 	// maeに残りのdata[i]をくっつける。
-	for (index_ += srclen; index_ < sizeof(data[i]) / sizeof(char); index_++)
+	for (;/* index_ < len*/; index_++)
 	{
-		mae[index_ + srclen] = data[index_];
+		if (data[index_] != '\0') {
+			mae[index_ + strlen + 1] = data[index_ + 1];
+		}
+		else {
+			break;
+		}
 	}
-	// help!!!!!
+	mae[index_ + strlen] = '\0';
+	printf("%s←\n", mae);
 
+	// help!!!!!
 	SHA256_CTX ctx;
 	sha256_init(&ctx);
-	sha256_update(&ctx, (unsigned char*)mae, len + srclen);
-	BYTE* digest = (BYTE*)malloc(64 * sizeof(BYTE));
+	printf("%i\n", index_ + strlen);
+	sha256_update(&ctx, mae, index_ + strlen);
+	BYTE digest[64];
 	for (int a = 0; a < 64; a++)
 	{
 		digest[a] = 0xff;
 	}
-
 	sha256_final(&ctx, (digest));
-	char* string = (char*)malloc(70);
-	int k;
-	for (int a = 0, k = 0; a < 32; a++, k += 2)
-	{
-		string[k] = (char)digest[a];
+	//result: ,nans&hash(sha256),nans&hash(sha256)
+	//TODO
+	printf("%c\r\n",digest[0]);
+	result[i * (16 + 2 + 9)] = ',';
+	for (int a = i * (16 + 2 + 9) + 1, int b = 0; b < strlen; b++, a++) {
+		result[a] = str[b];
 	}
-
-	
-	for (int a = 0; a<70;a++) {
-		result[a] = string[a];
+	result[i * (16 + 2 + 9)+strlen+1] = '&';
+	for (int a = i * (16 + 4 + 9) + strlen + 2, int b = 0; b < 32; b++,a++) {
+		result[a] = digest[b];
 	}
 }
 int main(int argc, char** argv)
 {
+	//source: src_block
+	// 　　　　不定長
+	//result: hash(sha256)&nans
+	//      2進化10進数整数(10byte)　32byte
+	//　　　　↑固定長↑
+	int threads = 10;
 	cudaSetDevice(0);
-	char* data1 = "previous_hash,addr,,1";
-	char* dev_data;
+	BYTE* data1 = reinterpret_cast<BYTE*>("00000193ab920406e5586e1a99472d573138e27191ca77ca897dc400f9abc8b8,b0bb15df4e3b489c5601b6a9c2d1aea66396b992653f547c22662a69e21bc8ec,,101,1657528305862");
+	BYTE* dev_data;
 	BYTE* dev_result_str;
-	long long int* dev_result;
-	cudaMallocManaged((void**)&dev_data, 100 * sizeof(char));
-	cudaMallocManaged((void**)&dev_result, sizeof(long long int));
-	cudaMallocManaged((void**)&dev_result_str, sizeof(char*) * 70);
-	cudaMemcpy(dev_data, data1, 100 * sizeof(char), cudaMemcpyHostToDevice);
-	char* buff = (char*)malloc(sizeof(long long int));
-	sha256_cuda << <1, 1 >> > (dev_data, 22, dev_result, dev_result_str);
+	cudaMallocManaged((void**)&dev_data, 600 * sizeof(BYTE));
+	cudaMallocManaged((void**)&dev_result_str, (32 + 2 + 9) * threads * sizeof(BYTE));
+	cudaMemcpy(dev_data, data1, sizeof(BYTE) * 600, cudaMemcpyHostToDevice);
+	sha256_cuda << <1, threads >> > (dev_data, 600, dev_result_str);
 	cudaDeviceSynchronize();
-	long long int *result;
-
-	result = (long long int *)malloc(1 * sizeof(long long int));
-	cudaMemcpy(result, dev_result, sizeof(long long int), cudaMemcpyDeviceToHost);
-	printf("%lld\n", result[0]);
 	BYTE* result_str;
-	result_str = (BYTE*)malloc(70);
-	cudaMemcpy(result_str, dev_result_str, 70, cudaMemcpyDeviceToHost);
-	printf( hash_to_string(result_str));
+	result_str = (BYTE*)malloc((16 + 2 + 9) * threads * sizeof(BYTE));
+	cudaMemcpy(result_str, dev_result_str, (16 + 2 + 9) * threads * sizeof(BYTE), cudaMemcpyDeviceToHost);
+	for(int a = 0; a < (16 + 2 + 9) * threads; a++)
+	{
+		printf("%c", result_str[a]);
+	}
 	return 0;
 }
