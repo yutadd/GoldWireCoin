c364e78f051594d3b83544fe9f39f5e008ed6f33
yutadd <spyk4m49@gmail.com>
2022-10-24 16:28:20 +0900

    fix shell script

--- GoldWireCoin_Linux.sh
+++ GoldWireCoin_Linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ./target/
-mv -f GoldWireCoin-0.1-jar-with-dependencies.jar ./GWC/GWC.jar
-cd ./GWC/
+mv -f GoldWireCoin-0.1-jar-with-dependencies.jar ../env/GWC.jar
+cd ../env/
 java -jar GWC.jar
